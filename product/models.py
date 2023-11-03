4ddd34914e295cbd640a1c9104b20648d9b248be
Isaac Finotti <imisaac2807@gmail.com>
2023-11-03 20:46:14 -0300

    Resizing images sent to make a product

--- product/models.py
+++ product/models.py
@@ -1,4 +1,7 @@
+from django.conf import settings
 from django.db import models
+from PIL import Image
+import os
 
 # Create your models here.
 
@@ -20,3 +23,28 @@ class Product(models.Model):
             ('S', 'Simple'),
         ),
     )
+
+    @staticmethod  # <- there is no 'self' in the method
+    def resize_image(img, new_width=800):
+        img_full_path = os.path.join(settings.MEDIA_ROOT, img.name)
+        img_pil = Image.open(img_full_path)
+        original_width, original_height = img_pil.size
+
+        if original_width <= new_width:
+            img_pil.close()
+            return
+
+        new_height = round((new_width * original_height) / original_width)
+        new_img = img_pil.resize((new_width, new_height), Image.LANCZOS)
+        new_img.save(img_full_path, optimize=True, quality=50)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        max_img_size = 800
+
+        if self.image:
+            self.resize_image(self.image, max_img_size)
+
+    def __str__(self):
+        return self.name
