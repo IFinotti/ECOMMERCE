34fae80302f6c7beaf8dba3e851b3feb61926834
Isaac Finotti <imisaac2807@gmail.com>
2023-11-03 21:00:22 -0300

    Creating the 'Variation' model

--- product/models.py
+++ product/models.py
@@ -48,3 +48,14 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, blank=True, null=True)
+    price = models.FloatField()
+    promotional_price = models.FloatField(default=0)
+    stock = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.name or self.product.name
