ac37283573bc0927c3035b70f32d3d9a31af0425
Isaac Finotti <imisaac2807@gmail.com>
2023-11-03 21:24:01 -0300

    Creating the inline tabular inside the product model

--- product/admin.py
+++ product/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-admin.site.register(models.Product)
+
+
+class InlineVariation(admin.TabularInline):
+    model = models.Variation
+    extra = 1
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        InlineVariation
+    ]
+
+
+admin.site.register(models.Product, ProductAdmin)
 admin.site.register(models.Variation)
