ca319ad63500b2b895969015ef9ec293f9b77685
Isaac Finotti <imisaac2807@gmail.com>
2023-11-04 21:52:36 -0300

    Starting the order model

--- order/models.py
+++ order/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    total = models.FloatField()
+    status = models.CharField(
+        choices=(
+            ('A', 'Approved'),
+            ('C', 'Created'),
+            ('R', 'Disapproved'),
+            ('', ''),
+        ),
+    )
