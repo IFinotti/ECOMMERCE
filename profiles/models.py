60dd202b1c5ddf163d8f43416df7e63e81848592
Isaac Finotti <imisaac2807@gmail.com>
2023-11-05 20:46:57 -0300

    Finishing the profile model

--- profiles/models.py
+++ profiles/models.py
@@ -6,12 +6,18 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     age = models.PositiveIntegerField()
-    birth_date = models
-    cpf = models
-    address = models
-    number = models
-    complement = models
-    hood = models
-    cep = models
-    city = models
-    state = models
+    birth_date = models.DateField()
+    cpf = models.CharField(max_length=11)
+    address = models.CharField(max_length=50)
+    number = models.CharField(max_length=5)
+    complement = models.CharField(max_length=30)
+    hood = models.CharField(max_length=30)
+    cep = models.CharField(max_length=8)
+    city = models.CharField(max_length=30)
+    state = models.CharField(
+        max_length=2,
+        default='SP',
+        choices=(
+
+        )
+    )
