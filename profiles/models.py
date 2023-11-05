9735227f7e9085d86fde5857a82ac885aa209a76
Isaac Finotti <imisaac2807@gmail.com>
2023-11-05 20:50:50 -0300

    Adding str method

--- profiles/models.py
+++ profiles/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     age = models.PositiveIntegerField()
     birth_date = models.DateField()
@@ -47,3 +48,9 @@ class Profile(models.Model):
             ('TO', 'Tocantins'),
         )
     )
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
+
+    def clean(self):
+        pass
