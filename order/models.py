5800d9a9ead6fa4ef9b0ea7b6199697000ce4e54
Isaac Finotti <imisaac2807@gmail.com>
2023-11-04 21:54:26 -0300

    Finishing the order model

--- order/models.py
+++ order/models.py
@@ -8,10 +8,14 @@ class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     total = models.FloatField()
     status = models.CharField(
+        default="C",
+        max_length=1,
         choices=(
             ('A', 'Approved'),
             ('C', 'Created'),
             ('R', 'Disapproved'),
-            ('', ''),
+            ('P', 'Pending'),
+            ('S', 'Sent'),
+            ('F', 'Finished'),
         ),
     )
