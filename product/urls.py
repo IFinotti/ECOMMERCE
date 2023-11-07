f63a0011fa9cbbbb879592dc131c32ab6101d618
Isaac Finotti <imisaac2807@gmail.com>
2023-11-07 12:52:40 -0300

    Creating the urls and the views of em

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,14 @@
+from ast import Add
 from django.urls import path
 from . import views
 
 app_name = 'product'
 
 urlpatterns = [
+    path('removefromcart/', views.RemoveFromCart.as_view(), name='removefromcart')
+    path('addtocart/', views.AddToCart.as_view(), name='addtocart')
+    path('<slug>', views.ProductDetail.as_view(), name='detail')
+    path('finish/', views.Finish.as_view(), name='finish')
     path('', views.ProductList.as_view(), name='list')
+    path('cart/', views.Cart.as_view(), name='cart')
 ]
--- product/views.py
+++ product/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
-
+from django.views.generic.list import ListView
+from django.views import View
 # Create your views here.
+
+
+class ProductList(ListView):
+    ...
+
+
+class ProductDetail(View):
+    ...
+
+
+class AddToCart(View):
+    ...
+
+
+class RemoveFromCart(View):
+    ...
+
+
+class Cart(View):
+    ...
+
+
+class Finish(View):
+    ...
