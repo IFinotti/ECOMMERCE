3871d5b186119c85604e498b8cbd6c2c011c557b
Isaac Finotti <imisaac2807@gmail.com>
2023-11-07 13:33:00 -0300

    Creating all the urls of the application

--- order/urls.py
+++ order/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from . import views
+
+app_name = 'order'
 
 urlpatterns = [
-
+    path('', views.Pay.as_view(), name='pay')
+    path('finishorder/', views.FinishOrder.as_view(), name='finishorder')
+    path('detail/', views.Detail.as_view(), name='detail')
 ]
--- order/views.py
+++ order/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views import View
 
 # Create your views here.
+
+
+class Pay(View):
+    ...
+
+
+class FinishOrder(View):
+    ...
+
+
+class Detail(View):
+    ...
