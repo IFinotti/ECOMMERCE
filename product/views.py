213217133b7022dab5855bcd3f4025b0c84194bd
Isaac Finotti <imisaac2807@gmail.com>
2023-11-12 13:05:23 -0300

    Coding the add to cart view and logic

--- product/views.py
+++ product/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404  # type:ignore
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views import View
+from django.contrib import messages
 from django.http import HttpResponse
 from . import models
 # Create your views here.
@@ -23,7 +24,33 @@ class ProductDetail(DetailView):
 
 class AddToCart(View):
     def get(self, *args, **kwargs):
-        return HttpResponse('AddToCart')
+        http_referer = self.request.META.get(
+            'HTTP_REFERER', reverse('product:list')
+        )
+        variation_id = self.request.GET.get('vid')
+
+        if not variation_id:
+            messages.error(
+                self.request,
+                'This product does not exist.'
+            )
+            return redirect(http_referer)
+        variation = get_object_or_404(models.Variation, id=variation_id)
+
+        # this 'if' statement check if a cart exists in a client account
+        if not self.request.session.get('cart'):
+            self.request.session['cart'] = {}
+            self.request.session.save()
+
+        cart = self.request.session['cart']
+        if variation_id in cart:
+            # TODO: variation exists in the cart
+            pass
+        else:
+            # TODO: variation does not exist in the cart
+            pass
+
+        return HttpResponse()
 
 
 class RemoveFromCart(View):
