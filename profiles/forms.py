0131370417be1a7978f044b73aa966298a217c1a
Isaac Finotti <imisaac2807@gmail.com>
2023-11-20 15:57:09 -0300

    Forms

--- profiles/forms.py
+++ profiles/forms.py
@@ -8,6 +8,9 @@ class UserForm(forms.ModelForm):
     password = forms.CharField(required=False, widget=forms.PasswordInput())
     # to hide the password on the form
 
+    password2 = forms.CharField(
+        required=False, widget=forms.PasswordInput(), label='Confirm password')
+
     def __init__(self, user=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -20,12 +23,27 @@ class UserForm(forms.ModelForm):
             'last_name',
             'username',
             'password',
+            'password2',
             'email',
         )
 
     def clean(self, *args, **kwargs):
         data = self.data
         cleaned = self.cleaned_data
+        validation_error_msgs = {}
+        user_data = cleaned.get('username')
+        password_data = cleaned.get('password')
+        email_data = cleaned.get('email')
+
+        user_db = User.objects.filter(username=user_data).first()
+
+        if self.user:
+            pass
+        else:
+            pass
+
+        if validation_error_msgs:
+            raise (forms.ValidationError(validation_error_msgs))
 
 
 class ProfileForm(forms.ModelForm):
