9131288107ea96c6d9b11dfc25709cbad0e008a2
Isaac Finotti <imisaac2807@gmail.com>
2023-12-11 15:52:19 -0300

    Creating the Dispatch class

--- order/views.py
+++ order/views.py
@@ -12,7 +12,14 @@ from .models import Order, OrderItem
 # Create your views here.
 
 
-class Pay(DetailView):
+class DispatchLoginRequired(View):
+    def dispatch(self, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            return redirect('profiles:create')
+        return super().dispatch(*args, **kwargs)
+
+
+class Pay(DispatchLoginRequired, DetailView):
     template_name = 'order/pay.html'
     model = Order
     pk_url_kwarg = 'pk'
