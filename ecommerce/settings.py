cec5af36a3b7987c6c35e8249529d77ef6ad247d
Isaac Finotti <imisaac2807@gmail.com>
2024-01-30 21:29:04 -0300

    Adding requirements.txt

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0mnycn&4x*do6qk7o^8im&ey#nr5b_ehyz@_y9j5z(tl6@$$ba
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
